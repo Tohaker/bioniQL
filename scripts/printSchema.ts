ab0b598426df3b13e90e97d5878ab453ee45e9e3
Miles Bardon <milesbardon@gmail.com>
2024-09-22 12:39:25 +0000

    refactor: Fixing type errors in printSchema function

--- scripts/printSchema.ts
+++ scripts/printSchema.ts
@@ -1,6 +1,6 @@
 import { writeFileSync } from "fs";
 import { printSchema, lexicographicSortSchema } from "graphql";
-import { schema } from "../src/schema";
+import { schema } from "../src/schema/index.js";
 
 const schemaAsString = printSchema(lexicographicSortSchema(schema));
 
